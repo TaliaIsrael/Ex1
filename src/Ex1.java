4fbd5466a1dc7d7e922c5b105b24ffc3707ca048
TaliaIsrael <taliaisrael.012@gmail.com>
2024-12-09 15:42:51 +0200

    number@Int Test

--- src/Ex1.java
+++ src/Ex1.java
@@ -11,82 +11,18 @@
  * You should implement the following static functions:
  */
 public class Ex1 {
-        /**
-         * Convert the given number (num) to a decimal representation (as int).
-         * It the given number is not in a valid format returns -1.
-         * @param "num" a String representing a number in basis [2,16]
-         * @return
-         */
+    /**
+     * Convert the given number (num) to a decimal representation (as int).
+     * It the given number is not in a valid format returns -1.
+     *
+     * @param "num" a String representing a number in basis [2,16]
+     * @return
+     */
 
-        public static int power(int n, int k) {
-            if (k == 0)
-                return 1;
-            return power(n, k - 1) * n;
-        }
-
-    public static int leToNum(char num)
-    {
-        int add = 10;
-        char[] nums = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
-
-        for (int j = 0; j < nums.length; j++) {
-            if (num == nums[j])
-            {
-                add = add + j;
-                break;
-            }
-            //if(j==nums.length-1)
-             //   add = Char
-        }
-        return add;
-        }
-
-    public static String[] splitNum(String num) {
-        String[] nums = num.split("b");
-        return nums;
-    }
-
-    public static int number2Int(String num) {
-        int ans = -1;
-        if (!isNumber(num)) {
-            return ans;
-       }
-       // if(num.)
-        ans = 0;
-        String[] nums = splitNum(num);
-        String snumber = nums[0];
-        String sbase = nums[1];
-        int s = 0;
-        char base1 = sbase.charAt(0);
-
-            for (int i = 0; i < snumber.length(); i++)
-            {
-                if (Character.isDigit(base1))
-                {
-                    if (Character.isDigit(snumber.charAt(i)))
-                    {
-                        ans = ans + Character.getNumericValue(snumber.charAt(i))*power(Integer.parseInt(sbase), (snumber.length() - 1 - i));
-                    }
-                    else
-                    {
-                        s = leToNum(snumber.charAt(i));
-                        ans = ans + s * power(Integer.parseInt(sbase), (snumber.length() - i - 1));
-                    }
-                }
-                else
-                {
-                    if (Character.isDigit(snumber.charAt(i)))
-                    {
-                        ans = ans + Character.getNumericValue(snumber.charAt(i)) * power(leToNum(base1), (snumber.length() - 1 - i));
-                    }
-                    else
-                    {
-                        s = leToNum(snumber.charAt(i));
-                        ans = ans + s * power(leToNum(base1), (snumber.length() - i - 1));
-                    }
-                }
-            }
-        return ans;
+    public static int power(int n, int k) {
+        if (k == 0)
+            return 1;
+        return power(n, k - 1) * n;
     }
 
     public static boolean StIsNumber(String num)
@@ -102,46 +38,100 @@ public class Ex1 {
         return isNum;
     }
 
-    /**
-         * This static function checks if the given String (g) is in a valid "number" format.
-         * @param a a String representing a number
-         * @return true iff the given String is in a number format
-         */
+    public static int leToNum(char num) {
+        int add = 10;
+        char[] nums = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
 
+        for (int j = 0; j < nums.length; j++) {
+            if (num == nums[j]) {
+                add = add + j;
+                break;
+            }
+        }
+        return add;
+    }
 
-        public static boolean isNumber(String a)
+    public static String[] splitNum(String num) {
+        String[] nums = num.split("b");
+        return nums;
+    }
+
+    public static int number2Int(String num) {
+        int ans = -1;
+        //if (!isNumber(num)) {
+          //  return ans;
+       // }
+        if(StIsNumber(num))
         {
-            boolean ans = true;
-            String[] nums = new String[2];
-            nums = splitNum(a);
-            String snumber = nums[0];
-            String sbase = nums[1];
-            char[] numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
-            boolean c = false;
-            for (int i = 0; i < snumber.length(); i++)                                  //לולאה  שבודקת האם יש במספר הנתון תו שלא קיים במערך ובהתאם לכך מכניסה ערך אמת או שקר לc
-            {
-                for (int j = 0; j < numbers.length; j++) {
-                    if (snumber.charAt(i) == numbers[j]) {
-                        c = true;
-                        break;
-                    }
+            return Integer.parseInt(num);
+        }
+        ans = 0;
+        String[] nums = splitNum(num);
+        String snumber = nums[0];
+        String sbase = nums[1];
+        int s = 0;
+        char base1 = sbase.charAt(0);
+
+        for (int i = 0; i < snumber.length(); i++) {
+            if (Character.isDigit(base1)) {
+                if (Character.isDigit(snumber.charAt(i))) {
+                    ans = ans + Character.getNumericValue(snumber.charAt(i)) * power(Integer.parseInt(sbase), (snumber.length() - 1 - i));
+                } else {
+                    s = leToNum(snumber.charAt(i));
+                    ans = ans + s * power(Integer.parseInt(sbase), (snumber.length() - i - 1));
                 }
-                if (!c) {
+            } else {
+                if (Character.isDigit(snumber.charAt(i))) {
+                    ans = ans + Character.getNumericValue(snumber.charAt(i)) * power(leToNum(base1), (snumber.length() - 1 - i));
+                } else {
+                    s = leToNum(snumber.charAt(i));
+                    ans = ans + s * power(leToNum(base1), (snumber.length() - i - 1));
+                }
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * This static function checks if the given String (g) is in a valid "number" format.
+     *
+     * @param a a String representing a number
+     * @return true iff the given String is in a number format
+     */
+
+
+    public static boolean isNumber(String a) {
+        boolean ans = true;
+        String[] nums = new String[2];
+        nums = splitNum(a);
+        String snumber = nums[0];
+        String sbase = nums[1];
+        char[] numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        boolean c = false;
+        for (int i = 0; i < snumber.length(); i++)                                  //לולאה  שבודקת האם יש במספר הנתון תו שלא קיים במערך ובהתאם לכך מכניסה ערך אמת או שקר לc
+        {
+            for (int j = 0; j < numbers.length; j++) {
+                if (snumber.charAt(i) == numbers[j]) {
+                    c = true;
                     break;
                 }
             }
-            if (a.isEmpty() || a.indexOf("b") == -1 || sbase == snumber || Integer.parseInt(nums[1]) > 10 || a.contains(" ") || !c || a.contains("bb") )
-                ans = false;
-            else {
-                for (int j = 0; j < numbers.length; j++) {                             //לולאה  שבודקת האם הבסיס הנתון הוא תו שקיים במערך, אם לא מכניסה לans שקר
-                    if (sbase.charAt(0) != numbers[j] && j == (numbers.length - 1)) {
-                        ans = false;
-                    }
+            if (!c) {
+                break;
+            }
+        }
+        if (a.isEmpty() || a.indexOf("b") == -1 || sbase == snumber || Integer.parseInt(nums[1]) > 10 || a.contains(" ") || !c || a.contains("bb"))
+            ans = false;
+        else {
+            for (int j = 0; j < numbers.length; j++) {                             //לולאה  שבודקת האם הבסיס הנתון הוא תו שקיים במערך, אם לא מכניסה לans שקר
+                if (sbase.charAt(0) != numbers[j] && j == (numbers.length - 1)) {
+                    ans = false;
                 }
             }
-            return ans;
-
         }
+        return ans;
+
+    }
 
     public static boolean goodBase(String base)                                    //פונקציה שמקבלת מחרוזת ומחזירה אמת אם המחרוזת יכולה להיות בסיס, אחרת מחזירה שקר
     {
@@ -162,53 +152,53 @@ public class Ex1 {
             return ans;
         }
     }
-        /**
-         * Calculate the number representation (in basis base)
-         * of the given natural number (represented as an integer).
-         * If num<0 or base is not in [2,16] the function should return "" (the empty String).
-         * @param num the natural number (include 0).
-         * @param base the basis [2,16]
-         * @return a String representing a number (in base) equals to num, or an empty String (in case of wrong input).
-         */
-        public static String int2Number(int num, int base) {
-            String ans = "";
-           if(num<0 || base<2 || base>16)
-           {
-               ans = "";
-           }
-           else {
 
-           }
-            return ans;
+    /**
+     * Calculate the number representation (in basis base)
+     * of the given natural number (represented as an integer).
+     * If num<0 or base is not in [2,16] the function should return "" (the empty String).
+     *
+     * @param num  the natural number (include 0).
+     * @param base the basis [2,16]
+     * @return a String representing a number (in base) equals to num, or an empty String (in case of wrong input).
+     */
+    public static String int2Number(int num, int base) {
+        String ans = "";
+        if (num < 0 || base < 2 || base > 16) {
+            ans = "";
+        } else {
+
         }
+        return ans;
+    }
 
-        /**
-         * Checks if the two numbers have the same value.
-         * @param n1 first number
-         * @param n2 second number
-         * @return true iff the two numbers have the same values.
-         */
-        public static boolean equals(String n1, String n2) {
-            boolean ans = true;
+    /**
+     * Checks if the two numbers have the same value.
+     *
+     * @param n1 first number
+     * @param n2 second number
+     * @return true iff the two numbers have the same values.
+     */
+    public static boolean equals(String n1, String n2) {
+        boolean ans = true;
 
 
+        return ans;
+    }
 
-            return ans;
-        }
+    /**
+     * This static function search for the array index with the largest number (in value).
+     * In case there are more than one maximum - returns the first index.
+     * Note: you can assume that the array is not null and is not empty, yet it may contain null or none-valid numbers (with value -1).
+     *
+     * @param arr an array of numbers
+     * @return the index in the array in with the largest number (in value).
+     */
+    public static int maxIndex(String[] arr) {
+        int ans = 0;
+        // add your code here
 
-        /**
-         * This static function search for the array index with the largest number (in value).
-         * In case there are more than one maximum - returns the first index.
-         * Note: you can assume that the array is not null and is not empty, yet it may contain null or none-valid numbers (with value -1).
-         * @param arr an array of numbers
-         * @return the index in the array in with the largest number (in value).
-         *
-         */
-        public static int maxIndex(String[] arr) {
-            int ans = 0;
-            // add your code here
-
-            ////////////////////
-            return ans;
-        }
+        ////////////////////
+        return ans;
+    }
 }
